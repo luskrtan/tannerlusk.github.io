82cb7ccbb480482fdd754a145dc06ba3549760d7
luskrtan <77557050+luskrtan@users.noreply.github.com>
2021-04-27 20:16:30 -0600

    Fix updated time

--- js/date.js
+++ js/date.js
@@ -1,11 +1,1 @@
-let options = {
-    weekday: "long",
-    day: "numeric" ,
-    month: "long", 
-    year: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric"
-};
-
-document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US", options);
+document.getElementById("currentdate").innerHTML = document.lastModified;

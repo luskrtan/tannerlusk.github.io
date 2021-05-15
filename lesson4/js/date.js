2b25172404419a462a5956a8824686398a3b5ba8
luskrtan <77557050+luskrtan@users.noreply.github.com>
2021-05-14 20:40:42 -0600

    fixed footer

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,1 +1,5 @@
-document.getElementById("currentdate").innerHTML = document.lastModified;
+let currDate = new Date();
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+document.getElementById("currentdate").innerHTML = days[currDate.getDay()] + ", " + currDate.getDate() + " "
++ months[currDate.getMonth()] + " " + currDate.getFullYear();

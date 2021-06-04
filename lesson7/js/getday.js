fab8170f1c0190e5364c29e7e1756feadfb80166
luskrtan <77557050+luskrtan@users.noreply.github.com>
2021-06-04 00:48:42 -0600

    localStorage

--- lesson7/js/getday.js
+++ lesson7/js/getday.js
@@ -1,7 +1,18 @@
 let d = new Date();
 var dayOfWeek = d.getDay();
 var today = document.getElementById("pancakeBanner");
+var newTime = d.getTime();
 
+if(localStorage.getItem('timeInput') !== null) {
+    var pastTime = localStorage.getItem('timeInput');
+    localStorage.setItem('timeInput', newTime.toString());
+    var dayDifference = Math.floor((newTime - pastTime) / (1000*3600 *24));
+    document.getElementById('daysSinceVisit').innerHTML = "Days Since Last Visit: " + dayDifference + " ";
+}
+else {
+    localStorage.setItem('timeInput', newTime.toString());
+    document.getElementById('daysSinceVisit').innerHTML = "Days Since Last Visit: 0 ";
+}
 
 if (dayOfWeek == 5){
     today.style.display = "block";

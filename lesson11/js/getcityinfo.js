edac01d526fa18c33369ad7f3167f8345387fa20
luskrtan <77557050+luskrtan@users.noreply.github.com>
2021-07-14 22:28:02 -0600

    maybe done

--- lesson11/js/getcityinfo.js
+++ lesson11/js/getcityinfo.js
@@ -1,81 +1,87 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+let siteName = document.title;
+const cityID = [0,2,6];
 
-fetch(requestURL)
-  .then(function (response) {
-      return response.json();
-      
+
+if (siteName === 'Southeast Idaho Weather Watch'){
+  const cityClass = ['sodaSprings','fishHaven','preston'];
+  fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+        
+    })
+    .then(function (jsonObject){
+      const towns = jsonObject['towns'];
+        console.table(jsonObject);
+            for (var i = 0; i < 3; i++){
+            let card = document.createElement('section');
+            let name = document.createElement('h2');
+            let motto = document.createElement('h3');
+            let year = document.createElement('p');
+            let population = document.createElement('p');
+            let rain = document.createElement('p');
+            
+
+            name.textContent = towns[cityID[i]].name;
+            motto.textContent = towns[cityID[i]].motto;
+            year.textContent = 'Year Founded: ' + towns[cityID[i]].yearFounded;
+            population.textContent = 'Population: ' + towns[cityID[i]].currentPopulation;
+            rain.textContent =  'Annual Rain Fall: ' + towns[cityID[i]].averageRainfall;
+
+            card.appendChild(name);
+            card.appendChild(motto);
+            card.appendChild(year);
+            card.appendChild(population);
+            card.appendChild(rain);
+
+            document.querySelector('article.' + cityClass[i] +'HomeInfo').appendChild(card);
+            }
+    })
+}
+else{
+  var cityNum;
+  switch (siteName){
+    case 'Preston Home Page':
+      cityNum = cityID[2];
+      break;
+    case 'Fish Haven Home Page':
+      cityNum = cityID[1];
+      break;
+    case 'Soda Springs Home Page':
+      cityNum = cityID[0];
+      break;
+  }
+  fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+        
+    })
+    .then(function (jsonObject){
+      const towns = jsonObject['towns'];
+        console.table(jsonObject);
+        let card = document.createElement('section');
+        let event1 = document.createElement('p');
+        let event2 = document.createElement('p');
+        let event3 = document.createElement('p');
+        var event4;
+        if (siteName === 'Fish Haven Home Page'){
+          event4 = document.createElement('p');
+        }
+
+        event1.textContent = towns[cityNum].events[0];
+        event2.textContent = towns[cityNum].events[1];
+        event3.textContent = towns[cityNum].events[2];
+        if (siteName === 'Fish Haven Home Page'){
+          event4.textContent = towns[cityNum].events[3];
+        }
+
+        card.appendChild(event1);
+        card.appendChild(event2);
+        card.appendChild(event3);
+        if (siteName === 'Fish Haven Home Page'){
+          card.appendChild(event4);
+        }
+
+        document.querySelector('article.upcomingevents').appendChild(card);
   })
-  .then(function (jsonObject){
-    const towns = jsonObject['towns'];
-      console.table(jsonObject);
-          //Code for Preston
-          let pCard = document.createElement('section');
-          let pName = document.createElement('h2');
-          let pMotto = document.createElement('h3');
-          let pYear = document.createElement('p');
-          let pPopulation = document.createElement('p');
-          let pRain = document.createElement('p');
-          
-
-          pName.textContent = towns[6].name;
-          pMotto.textContent = towns[6].motto;
-          pYear.textContent = 'Year Founded: ' + towns[6].yearFounded;
-          pPopulation.textContent = 'Population: ' + towns[6].currentPopulation;
-          pRain.textContent =  'Annual Rain Fall: ' + towns[6].averageRainfall;
-
-          pCard.appendChild(pName);
-          pCard.appendChild(pMotto);
-          pCard.appendChild(pYear);
-          pCard.appendChild(pPopulation);
-          pCard.appendChild(pRain);
-
-          document.querySelector('article.prestonHomeInfo').appendChild(pCard);
-
-          //Code for Soda Springs
-          let ssCard = document.createElement('section');
-          let ssName = document.createElement('h2');
-          let ssMotto = document.createElement('h3');
-          let ssYear = document.createElement('p');
-          let ssPopulation = document.createElement('p');
-          let ssRain = document.createElement('p');
-          
-
-          ssName.textContent = towns[0].name;
-          ssMotto.textContent = towns[0].motto;
-          ssYear.textContent = 'Year Founded: ' + towns[0].yearFounded;
-          ssPopulation.textContent = 'Population: ' + towns[0].currentPopulation;
-          ssRain.textContent =  'Annual Rain Fall: ' + towns[0].averageRainfall;
-
-          ssCard.appendChild(ssName);
-          ssCard.appendChild(ssMotto);
-          ssCard.appendChild(ssYear);
-          ssCard.appendChild(ssPopulation);
-          ssCard.appendChild(ssRain);
-
-          document.querySelector('article.sodaSpringsHomeInfo').appendChild(ssCard);
-
-          //Code for Fish Haven
-          let fhCard = document.createElement('section');
-          let fhName = document.createElement('h2');
-          let fhMotto = document.createElement('h3');
-          let fhYear = document.createElement('p');
-          let fhPopulation = document.createElement('p');
-          let fhRain = document.createElement('p');
-          
-
-          fhName.textContent = towns[2].name;
-          fhMotto.textContent = towns[2].motto;
-          fhYear.textContent = 'Year Founded: ' + towns[2].yearFounded;
-          fhPopulation.textContent = 'Population: ' + towns[2].currentPopulation;
-          fhRain.textContent =  'Annual Rain Fall: ' + towns[2].averageRainfall;
-
-          fhCard.appendChild(fhName);
-          fhCard.appendChild(fhMotto);
-          fhCard.appendChild(fhYear);
-          fhCard.appendChild(fhPopulation);
-          fhCard.appendChild(fhRain);
-
-          document.querySelector('article.fishHavenHomeInfo').appendChild(fhCard);
-      
-      
-  })
+}
--- lesson11/js/getday.js
+++ lesson11/js/getday.js
@@ -1,7 +1,7 @@
 let d = new Date();
 var dayOfWeek = d.getDay();
 var dName = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun", "Mon", "Tues", "Wed", "Thur"];
-var today = document.getElementById("pancakeBanner");
+let siteTitle = document.title;
 
 document.getElementById("day1Name").innerHTML = dName[dayOfWeek];
 document.getElementById("day2Name").innerHTML = dName[dayOfWeek+1];
@@ -9,8 +9,11 @@ document.getElementById("day3Name").innerHTML = dName[dayOfWeek+2];
 document.getElementById("day4Name").innerHTML = dName[dayOfWeek+3];
 document.getElementById("day5Name").innerHTML = dName[dayOfWeek+4];
 
+if (siteTitle === 'Preston Home Page'){
+var today = document.getElementById("pancakeBanner");
 if (dayOfWeek == 5){
     today.style.display = "block";
 } else {
     today.style.display = "none";
+}
 }
